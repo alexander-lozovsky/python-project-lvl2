a22e82b01d9e46ce5132f25d08599104a83c55f3
Alexander Lozovsky <lozovsky.alx@yandex.ru>
2021-02-21 14:48:57 +0300

    fix lint errors

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,12 +1,16 @@
 import argparse
 
 
-
 def run():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output', metavar='FORMAT')
+    parser.add_argument(
+        '-f',
+        '--format',
+        help='set format of output',
+        metavar='FORMAT',
+    )
 
     parser.parse_args()
